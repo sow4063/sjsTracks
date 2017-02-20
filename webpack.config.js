adc7d0dcfea2bb638b363518e7b30c48b4b62c3e
Seunghoon Ko <imskojs@gmail.com>
2017-02-20 21:55:50 +0900

    (feat) webpack setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const PATHS = {
   app: path.resolve(__dirname, 'client', 'app'),
@@ -9,29 +10,52 @@ const PATHS = {
 module.exports = env => {
   return {
     entry: {
-      app: path.resolve(PATHS.app, 'root.component.js')
+      app: path.resolve(PATHS.app, 'root.module.js')
     },
     output: {
       path: PATHS.dist,
       filename: `[name].js`
     },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      compress: true,
+      port: 8080,
+      https: false 
+    },
     module: {
       rules: [
         {
-          test: /\.js$/,
-          exclude: `/node_modules/`,
-          use: `babel-loader`
-        },
-        {
           test: /\.html$/,
           use: [`ngtemplate-loader`, `html-loader`]
-        }
+        },
+        {
+          test: /(\.css|\.scss)$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              { loader: `css-loader`, options: { sourceMap: true } },
+              { loader: `sass-loader`, options: { sourceMap: true } },
+            ]
+          })
+        },
+        {
+          test: /\.js$/,
+          exclude: `/node_modules/`,
+          use: [`ng-annotate-loader`, `babel-loader`]
+        },
       ]
     },
     plugins: [
-       new CopyWebpackPlugin([
-         {from: path.resolve( PATHS.app, 'index.html' )}
-      ])
+      new CopyWebpackPlugin([ 
+        { 
+          from: path.resolve( PATHS.app, 'index.html' ) 
+        },
+        { 
+          context: path.resolve( PATHS.app, `img` ),
+          from: `**/*`,
+          to: path.resolve(PATHS.dist, `img`)
+        } 
+      ]),
+      new ExtractTextPlugin(`[name].css`)
     ]
   };
 }
