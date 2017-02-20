46eff0e0db423e2f4813769f00a385e06055bba9
Seunghoon Ko <imskojs@gmail.com>
2017-02-20 19:15:24 +0900

    (feat) working webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const PATHS = {
+  app: path.resolve(__dirname, 'client', 'app'),
+  dist: path.resolve(__dirname, 'dist')
 }
+
 module.exports = env => {
-    return {
-        entry: {
-            app: path.resolve(__dirname, 'client', 'app', 'root.module.js')
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: `[name].js`
+  return {
+    entry: {
+      app: path.resolve(PATHS.app, 'root.module.js')
+    },
+    output: {
+      path: PATHS.dist,
+      filename: `[name].js`
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          use: `babel-loader`
         }
-    };
+      ]
+    },
+    plugins: [
+       new CopyWebpackPlugin([
+         {from: path.resolve( PATHS.app, 'index.html' )}
+      ])
+    ]
+  };
 }
 
 
