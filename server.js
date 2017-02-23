772038c460b21300c3b4aaeca4455edc625c27d9
윤송이 <yunsong-i@yunsong-iui-MacBook-Pro.local>
2017-02-23 10:51:47 +0900

    a

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const router = require('./server/routes/router.js')
+const router = require('./server/routes/router.js');
+const MethodOverride = require('Method-override');
+const Multer = require('Multer');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 
 app.use('/', express.static(__dirname + '/dist'));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true });
+
 
 app.listen(port, () => console.log('Express listening on port', port))
