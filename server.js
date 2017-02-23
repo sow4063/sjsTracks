24e10842b3be481f2b43df52de1c2357093c0255
윤송이 <yunsong-i@yunsong-iui-MacBook-Pro.local>
2017-02-23 13:58:04 +0900

    modify multer spelling

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./server/routes/router.js');
 const methodOverride = require('method-override');
-const Multer = require('Multer');
+const Multer = require('multer');
 const bodyParser = require('body-parser');
 
 const app = express();
